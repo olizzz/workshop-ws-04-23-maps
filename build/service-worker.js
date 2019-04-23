f51693de3a4f16176cfad1096219ee938b067d39
olizzz <xinchen.zhao.20@dartmouth.edu>
2019-04-23 15:08:54 -0400

    added all

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.08f705ce22b7ebfd28485fbc6ab89948.js"
 );
 
 workbox.clientsClaim();
